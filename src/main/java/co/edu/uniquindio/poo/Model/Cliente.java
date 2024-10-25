3dacf2980c1db623cb857e5e070ce188d94bffe1
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2024-10-25 09:39:51 -0500

    Se crea la clase reserva

--- src/main/java/co/edu/uniquindio/poo/Model/Cliente.java
+++ src/main/java/co/edu/uniquindio/poo/Model/Cliente.java
@@ -3,11 +3,13 @@ package co.edu.uniquindio.poo.Model;
 public class Cliente {
     private String nombre;
     private String telfono;
+    private Reserva reserva;
 
 
-    public Cliente(String nombre, String telfono) {
+    public Cliente(String nombre, String telfono, Reserva reserva) {
         this.nombre = nombre;
         this.telfono = telfono;
+        this.reserva = reserva;
     }
 
 
@@ -30,7 +32,13 @@ public class Cliente {
         this.telfono = telfono;
     }
 
-    
 
-    
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }   
 }
