6ce29e94b25136a6828e85c832c1c00f1d96c802
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2024-10-25 09:19:03 -0500

    Se crea la clase vehiculo

---
+++ src/main/java/co/edu/uniquindio/poo/Model/Vehiculo.java
@@ -0,0 +1,52 @@
+package co.edu.uniquindio.poo.Model;
+
+import java.time.LocalDate;
+
+public abstract class Vehiculo {
+    public String numeroMatricula;
+    public String marca;
+    public String modelo;
+    public LocalDate anoFabricacion;
+    
+    public Vehiculo(String numeroMatricula, String marca, String modelo, LocalDate anoFabricacion) {
+        this.numeroMatricula = numeroMatricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacion = anoFabricacion;
+    }
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public LocalDate getAnoFabricacion() {
+        return anoFabricacion;
+    }
+
+    public void setAnoFabricacion(LocalDate anoFabricacion) {
+        this.anoFabricacion = anoFabricacion;
+    }
+    
+
+
+}
